4741a80aa312090d4f028ae785f502c47aa88a80
rpour <info@rpour.de>
2014-12-08 18:09:45 +0100

    WMS GetCapabilities import.

--- src/WhereGroup/MetadorBundle/Controller/ImportController.php
+++ src/WhereGroup/MetadorBundle/Controller/ImportController.php
@@ -10,6 +10,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
+use Rhumsaa\Uuid\Uuid;
+use Rhumsaa\Uuid\Exception\UnsatisfiedDependencyException;
+
 use WhereGroup\MetadorBundle\Entity\Metadata;
 use WhereGroup\MetadorBundle\Entity\Helptext;
 use WhereGroup\MetadorBundle\Entity\Address;
@@ -83,7 +86,7 @@ class ImportController extends Controller
             $this->get('session')->getFlashBag()->add('error', 'Bitte GetCapabilities URL angeben.');
             return $this->redirect($this->generateUrl('wheregroup_metador_default_index'));
         }
-        
+
         $conf = $this->container->getParameter('metador');
         $xml = file_get_contents($url);
 
@@ -95,22 +98,37 @@ class ImportController extends Controller
         $this->parser = new XmlParser($xml, new XmlParserFunctions());
 
         switch($version["version"]) {
-            case "1.1.1" :
+            case "1.1.1":
                 $this->parser->loadSchema(file_get_contents($conf['wmsimport']['path'] . 'wms_1-1-1.json'));
                 break;
-            case "1.3.0" :
+            case "1.3.0":
                 $this->parser->loadSchema(file_get_contents($conf['wmsimport']['path'] . 'wms_1-3-0.json'));
                 break;
         }
-         
+
         $array = $this->parser->parse();
 
-        die('<pre>' . print_r($array, 1) . '</pre>');
+        if (isset($array['p'])) {
 
-        // if (isset($array['p'])) {
-        //     $metadata = $this->get('metador_metadata');
-        //     $metadata->saveObject($p);
-        //     $this->get('session')->getFlashBag()->add('info', 'Erfolgreich importiert.');
-        // }
+            try {
+                $array['p']['fileIdentifier'] = Uuid::uuid4()->toString();
+                $array['p']['identifier'][0]['code'] = $array['p']['fileIdentifier'];
+                $array['p']['identifier'][0]['codespace'] = "";
+
+            } catch (UnsatisfiedDependencyException $e) {
+                $this->get('session')->getFlashBag()->add('error', 'UUID konnte nicht generiert werden.');
+                return $this->redirect($this->generateUrl('wheregroup_metador_default_index'));
+            }
+
+            $array['p']['dateStamp'] = date('Y-m-d');
+            $array['p']['hierarchyLevel'] = 'service';
+
+
+            $metadata = $this->get('metador_metadata');
+            $metadata->saveObject($array['p']);
+            $this->get('session')->getFlashBag()->add('info', 'Erfolgreich importiert.');
+        }
+
+        return $this->redirect($this->generateUrl('wheregroup_metador_default_index'));
     }
 }
