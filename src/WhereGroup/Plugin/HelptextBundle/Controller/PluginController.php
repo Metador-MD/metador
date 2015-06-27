368789247be986179f25f55127c4671e9e016d66
rpour <info@rpour.de>
2015-06-27 19:01:00 +0200

    Added plugin info page

---
+++ src/WhereGroup/Plugin/HelptextBundle/Controller/PluginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WhereGroup\Plugin\HelptextBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class PluginController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function viewAction()
+    {
+        return array();
+    }
+}
