03a9cbd2822cf77f30212268035e69c6d5a3a3e1
rpour <info@rpour.de>
2015-08-14 09:32:35 +0200

    teaser twig extension

---
+++ src/WhereGroup/CoreBundle/Twig/Extension/TeaserExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace WhereGroup\CoreBundle\Twig\Extension;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class TeaserExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            'teaser' => new \Twig_Filter_Method($this, 'teaser')
+        );
+    }
+
+    public function getName()
+    {
+        return 'teaser';
+    }
+
+
+    public function teaser($string, $maxLen = 100)
+    {
+        $teaser = '';
+        $count  = 0;
+        $words  = explode(' ', $string);
+
+        foreach ($words as $word) {
+            $count += strlen($word);
+
+            if ($count >= $maxLen) {
+                break;
+            }
+
+            $teaser += ' ' . $word;
+        }
+
+        return trim($teaser) . ' ...';
+    }
+}
