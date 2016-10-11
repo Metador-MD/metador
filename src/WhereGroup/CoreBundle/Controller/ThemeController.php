cac2a0287a812cc8ee4ede3dc1504ea12f7333cb
A. Rashid Pour <info@rpour.de>
2016-10-11 13:41:53 +0200

    Redesign

---
+++ src/WhereGroup/CoreBundle/Controller/ThemeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WhereGroup\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+/**
+ * Class ThemeController
+ * @package WhereGroup\CoreBundle\Controller
+ * @Route("/theme/")
+ */
+class ThemeController extends Controller
+{
+    /**
+     * @Route("/", name="metador_theme_index")
+     * @Method("GET")
+     * @Template("MetadorThemeBundle:Theme:index.html.twig")
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
