4c45f54020c24cd3965d02a34187385ee8611565
Paul Schell <paul.schell@wheregroup.com>
2016-10-11 14:59:17 +0200

    Fixed group rights for finder

--- src/WhereGroup/CoreBundle/Controller/HomeController.php
+++ src/WhereGroup/CoreBundle/Controller/HomeController.php
@@ -23,8 +23,11 @@ class HomeController extends Controller
         $params = $this->get('request_stack')->getCurrentRequest()->query->all();
         $filter = new Finder();
 
-        if (!$this->get('security.authorization_checker')->isGranted('ROLE_SYSTEM_GEO_OFFICE')) {
+        $filter->public = true;
+
+        if ($this->get('security.authorization_checker')->isGranted('ROLE_SYSTEM_GEO_OFFICE')) {
             $filter->geoOffice = true;
+            $filter->public = false;
         }
 
         $filter->page  = isset($params['page'])  ? $params['page']  : 1;
