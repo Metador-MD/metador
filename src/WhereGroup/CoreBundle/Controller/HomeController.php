2986bbc0a56efb02be13c0b3731cf0786c0e2dce
A. Rashid Pour <info@rpour.de>
2016-10-21 10:25:06 +0200

    Bugfix: visibility of private metadata

--- src/WhereGroup/CoreBundle/Controller/HomeController.php
+++ src/WhereGroup/CoreBundle/Controller/HomeController.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use WhereGroup\CoreBundle\Component\Finder;
+use WhereGroup\UserBundle\Entity\User;
 
 /**
  * @Route("/")
@@ -27,11 +29,23 @@ class HomeController extends Controller
 
         if ($this->get('security.authorization_checker')->isGranted('ROLE_SYSTEM_GEO_OFFICE')) {
             $filter->geoOffice = true;
+            $filter->public = null;
         }
 
         $filter->page  = isset($params['page'])  ? $params['page']  : 1;
         $filter->hits  = isset($params['hits'])  ? $params['hits']  : 10;
         $filter->terms = isset($params['terms']) ? $params['terms'] : '';
+
+        /** @var UsernamePasswordToken $token */
+        $token = $this->get('security.token_storage')->getToken();
+
+        /** @var User $user */
+        $user = $token->getUser();
+
+        if (is_object($user)) {
+            $filter->userId = $user->getId();
+            $filter->public = null;
+        }
 
         $metadata = $this->get('metadata')->find($filter);
 
