3ee1e3ee9cdc7876c2bdd7186cfbf5e4be1263f6
A. Rashid Pour <info@rpour.de>
2017-02-03 10:58:29 +0100

    Added doc

--- src/Plugins/WhereGroup/HelptextBundle/Controller/PluginController.php
+++ src/Plugins/WhereGroup/HelptextBundle/Controller/PluginController.php
@@ -5,6 +5,10 @@ namespace Plugins\WhereGroup\HelptextBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * Class PluginController
+ * @package Plugins\WhereGroup\HelptextBundle\Controller
+ */
 class PluginController extends Controller
 {
     /**
