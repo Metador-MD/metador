9a3bec80c5fdb07dfc1643997de0fd80c4405765
A. Rashid Pour <info@rpour.de>
2017-05-02 10:19:57 +0200

    Added first controller

---
+++ src/Plugins/WhereGroup/MapBundle/Controller/PluginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Plugins\WhereGroup\MapBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Class PluginController
+ * @package Plugins\WhereGroup\DatasetBundle\Controller
+ */
+class PluginController extends Controller
+{
+
+}
