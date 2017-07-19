3ce59bd6e9ea23db355c7c081dc42525a9dcd1cb
A. R. Pour <info@rpour.de>
2017-07-19 13:54:01 +0200

    Refactoring plugin key's and some cleanup.

--- src/Plugins/WhereGroup/DatasetBundle/ProfileDatasetBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Plugins\WhereGroup\DatasetBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ProfileDatasetBundle extends Bundle
-{
-}
--- src/Plugins/WhereGroup/HelptextBundle/MetadorHelptextBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Plugins\WhereGroup\HelptextBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MetadorHelptextBundle extends Bundle
-{
-}
