21b8ff8d3eccbbb13a273899b0225e9cebf9b2fe
A. R. Pour <info@rpour.de>
2017-07-29 00:20:30 +0200

    Added ArrayParser

---
+++ src/WhereGroup/CoreBundle/Component/Utils/ArrayParser.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace WhereGroup\CoreBundle\Component\Utils;
+
+/**
+ * Class ArrayParser
+ * @package WhereGroup\CoreBundle\Component\Utils
+ */
+class ArrayParser
+{
+    /**
+     * @param array $array
+     * @param $path
+     * @return array|null
+     */
+    public static function get(array $array, $path)
+    {
+        if (empty($path) || trim($path) === "") {
+            return $array;
+        }
+
+        return self::arrayGet($array, self::explodePath($path));
+    }
+
+    /**
+     * @param array $array
+     * @param $path
+     * @param $value
+     * @return mixed
+     */
+    public static function set(array &$array, $path, $value)
+    {
+        self::arraySet($array, self::explodePath($path), $value);
+
+        return $value;
+    }
+
+    /**
+     * @param array $array
+     * @param $path
+     * @return bool
+     */
+    public static function delete(array &$array, $path)
+    {
+        return self::arrayDelete($array, self::explodePath($path));
+    }
+
+    /**
+     * @param $array
+     * @param $path
+     * @return bool
+     */
+    public static function isEmpty($array, $path)
+    {
+        return empty(self::get($array, $path));
+    }
+
+    /**
+     * @param array $array
+     * @param $path
+     * @return array|int|null
+     */
+    public static function length(array $array, $path)
+    {
+        $item = self::get($array, $path);
+
+        if (is_array($item) || is_object($item)) {
+            return count((array)$item);
+        }
+
+        if (is_string($item)) {
+            return strlen($item);
+        }
+
+        if (is_numeric($item)) {
+            return $item;
+        }
+
+        return null;
+    }
+
+    /**
+     * @param $array
+     * @param $path
+     * @param null $find
+     * @return bool
+     */
+    public static function exists($array, $path, $find = null)
+    {
+        $result = self::get($array, $path);
+
+        if (!is_null($result) && is_null($find)) {
+            return true;
+        }
+
+        if ((is_array($result) && in_array($find, $result))
+            || (is_array($result) && isset($result[$find]))
+            || (is_string($result) && strstr($result, $find) !== false)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $array
+     * @param $path
+     * @param $value
+     * @return mixed
+     */
+    public static function append(array &$array, $path, $value)
+    {
+        $item = self::get($array, $path);
+
+        if ($item === null) {
+            $item = $value;
+        } elseif (is_array($item)) {
+            $item[] = $value;
+        } else {
+            $item .= $value;
+        }
+
+        return self::set($array, $path, $item);
+    }
+
+    /**
+     * @param array $array1
+     * @param array $array2
+     * @return array
+     */
+    public static function merge(array $array1, array $array2)
+    {
+        foreach ($array2 as $key => $val) {
+            if (array_key_exists($key, $array1) && is_array($val)) {
+                $array1[$key] = self::merge($array1[$key], $array2[$key]);
+                continue;
+            }
+
+            $array1[$key] = $val;
+        }
+
+        return $array1;
+    }
+
+    /**
+     * @param $array
+     * @param $keys
+     * @return null
+     */
+    private static function arrayGet($array, $keys)
+    {
+        $key = array_shift($keys);
+
+        if (isset($array[$key]) && count($keys) === 0) {
+            return $array[$key];
+        } elseif (isset($array[$key]) && count($keys) >= 1) {
+            return self::arrayGet($array[$key], $keys);
+        }
+
+        return null;
+    }
+
+    /**
+     * @param array $array
+     * @param $keys
+     * @param $value
+     * @return mixed
+     */
+    private static function arraySet(array &$array, $keys, $value)
+    {
+        $key = array_shift($keys);
+
+        if (count($keys) == 0) {
+            $array[$key] = $value;
+
+            return $value;
+        }
+
+        if (count($keys) > 0) {
+            if (!isset($array[$key])) {
+                $array[$key] = array();
+            }
+
+            self::arraySet($array[$key], $keys, $value);
+        }
+    }
+
+    /**
+     * @param array $array
+     * @param $keys
+     * @return bool
+     */
+    private static function arrayDelete(array &$array, $keys)
+    {
+        $key = array_shift($keys);
+
+        if (isset($array[$key]) && count($keys) == 0) {
+            unset($array[$key]);
+            return true;
+        }
+
+        if (isset($array[$key]) && count($keys) > 0) {
+            return self::arrayDelete($array[$key], $keys);
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $path
+     * @return array
+     */
+    private static function explodePath($path)
+    {
+        return explode(':', trim($path, ':'));
+    }
+}
---
+++ src/WhereGroup/CoreBundle/Tests/WhereGroup/CoreBundle/Component/Utils/ArrayParserTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace WhereGroup\Tests\WhereGroup\CoreBundle\Component\Utils;
+
+use PHPUnit\Framework\TestCase;
+use WhereGroup\CoreBundle\Component\Utils\ArrayParser;
+
+/**
+ * Class ArrayParserTest
+ * @package WhereGroup\Tests\WhereGroup\CoreBundle\Component\Utils
+ */
+class ArrayParserTest extends TestCase
+{
+    private $array;
+    private $types;
+
+    public function __construct($name = null, array $data = array(), $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $object = new \stdClass();
+        $object->a = 'a';
+        $object->b = 'b';
+        $object->c = 'c';
+
+        $this->types = array(
+            'array'  => array(1,2,3),
+            'object' => $object,
+            'string' => 'Hello',
+            'number' => 345
+        );
+
+        $this->array = array(
+            'types' => $this->types,
+            'user'  => array(
+                array('name' => 'Max'),
+                array('name' => 'Thomas'),
+                array('name' => 'Jochen')
+            )
+        );
+    }
+
+    public function testGet()
+    {
+        $this->assertEquals($this->types, ArrayParser::get($this->array, 'types'));
+        $this->assertEquals('Hello', ArrayParser::get($this->array, 'types:string'));
+        $this->assertEquals(345, ArrayParser::get($this->array, 'types:number'));
+        $this->assertEquals('Thomas', ArrayParser::get($this->array, 'user:1:name'));
+        $this->assertEquals(null, ArrayParser::get($this->array, 'i:dont:exist'));
+    }
+
+    public function testSet()
+    {
+        $array = $this->array;
+
+        // Adds number 4 to array.
+        // But normally you should use ArrayParser::append($array, 'types:array', 4); to do that.
+        ArrayParser::set($array, 'types:array:3', 4);
+        $this->assertEquals(array(1,2,3,4), ArrayParser::get($array, 'types:array'));
+
+        // Replaces the array with number 4.
+        ArrayParser::set($array, 'types:array', 4);
+        $this->assertEquals(4, ArrayParser::get($array, 'types:array'));
+    }
+
+    public function testDelete()
+    {
+
+    }
+
+    public function testIsEmpty()
+    {
+
+    }
+
+    public function testLength()
+    {
+        $this->assertEquals(3, ArrayParser::length($this->array, 'types:array'));
+        $this->assertEquals(3, ArrayParser::length($this->array, 'types:object'));
+        $this->assertEquals(5, ArrayParser::length($this->array, 'types:string'));
+        $this->assertEquals(345, ArrayParser::length($this->array, 'types:number'));
+    }
+
+    public function testExists()
+    {
+        $this->assertEquals(true, ArrayParser::exists($this->array, 'types:array'));
+        $this->assertEquals(true, ArrayParser::exists($this->array, 'types:array', 2));
+        $this->assertEquals(true, ArrayParser::exists($this->array, 'user:name', 'Thomas'));
+        $this->assertEquals(false, ArrayParser::exists($this->array, 'i:dont:exist'));
+    }
+
+    public function testAppend()
+    {
+        $array = $this->array;
+
+        ArrayParser::append($array, 'types:array', 4);
+        $this->assertEquals(array(1,2,3,4), ArrayParser::get($array, 'types:array'));
+    }
+
+    public function testMerge()
+    {
+
+    }
+}
