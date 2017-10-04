f02dc723bc0a9a74d4303d278d44dd8a01464a5a
A. Rashid Pour <info@rpour.de>
2017-10-04 14:01:20 +0200

    Ignore PSR2

--- src/WhereGroup/CoreBundle/Component/PDFExport.php
+++ src/WhereGroup/CoreBundle/Component/PDFExport.php
@@ -13,12 +13,14 @@ class PDFExport extends \TCPDF
         parent::__construct($orientation, $unit, $format, $unicode, $encoding, $diskcache, $pdfa);
     }
 
+    // @codingStandardsIgnoreLine
     public function Header()
     {
         $this->SetFont('helvetica', null, 12);
         $this->Image(__DIR__.'/../../ThemeBundle/Resources/public/img/logo-s.png', 170, 5, 30, '', 'PNG', '', 'T', false, 300, '', false, false, 1, false, false, false);
         $this->MultiCell(0, 10, $this->uuid, 0, 'L', false, 1, 20, 7, true, 10, false, true, 0, 'T', false);
     }
+
 
     public function setUUID($uuid)
     {
