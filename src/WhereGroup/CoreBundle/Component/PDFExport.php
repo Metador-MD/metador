0a43fb402e784134ec136e9041abe836ccb9c091
rpour <a.rashid.pour@wheregroup.com>
2020-11-16 15:32:34 +0100

    Added dompdf

--- src/WhereGroup/CoreBundle/Component/PDFExport.php
+++ src/WhereGroup/CoreBundle/Component/PDFExport.php
@@ -2,25 +2,11 @@
 
 namespace WhereGroup\CoreBundle\Component;
 
-require_once __DIR__ . '/../../../../vendor/tecnickcom/tcpdf/tcpdf.php';
+use Dompdf\Dompdf;
 
-class PDFExport extends \TCPDF
+class PDFExport
 {
     protected $uuid;
-
-    public function __construct($orientation, $unit, $format, $unicode, $encoding, $diskcache, $pdfa)
-    {
-        parent::__construct($orientation, $unit, $format, $unicode, $encoding, $diskcache, $pdfa);
-    }
-
-    // @codingStandardsIgnoreLine
-    public function Header()
-    {
-        //$this->SetFont('helvetica', null, 12);
-        //$this->Image(__DIR__.'/../../ThemeBundle/Resources/public/img/logo-s.png', 170, 5, 30, '', 'PNG', '', 'T', false, 300, '', false, false, 1, false, false, false);
-        //$this->MultiCell(0, 10, $this->uuid, 0, 'L', false, 1, 20, 7, true, 10, false, true, 0, 'T', false);
-    }
-
 
     public function setUUID($uuid)
     {
@@ -29,18 +15,11 @@ class PDFExport extends \TCPDF
 
     public function createPdf($html, $p)
     {
-        $this->SetCreator(PDF_CREATOR);
-        $this->SetAuthor('Metador');
-        $this->SetTitle($p['title']);
-        $this->SetSubject('Metadaten');
-        $this->SetFont('helvetica', '', 10);
-        $this->SetMargins(25, 12, 9);
-        $this->setPrintHeader(false);
-        $this->setPrintFooter(false);
-        $this->setAutoPageBreak(true, 20);
-        $this->AddPage();
-        $this->writeHTML($html, true, true, false, false, '');
-        $this->Output();
-//        $this->Output($p['_uuid'] . '.pdf', 'D');
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4');
+        $dompdf->render();
+        $dompdf->stream($p['_uuid'] . '.pdf', [ "Attachment" => false ]);
+        exit(0);
     }
 }
